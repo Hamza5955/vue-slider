2b04000a66c805f77755d714bc45ad5d2b8e2636
Hamza <Hamza5955>
2021-09-08 17:56:58 +0200

    fine

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-.
+Vue.config.devtools = true;
+const app = new Vue({
+  el: "#root",
+  data: {
+    contenPhoto: 0,
+    images: [
+      "img/image1.jpg",
+      "img/image2.jpg",
+      "img/image3.jpg",
+      "img/image4.jpg",
+    ],
+  },
+  methods: {
+    prev() {
+      this.contenPhoto--;
+      if (this.contenPhoto < 0) this.contenPhoto = this.images.length - 1;
+    },
+    next() {
+      this.contenPhoto++;
+      if (this.contenPhoto == this.images.length) this.contenPhoto = 0;
+    },
+  },
+  mounted() {
+    setInterval(() => {
+      this.next();
+    }, 3000);
+  },
+});
